38e4582cc51cff27c79cfe19cc2350eb77afa1b5
Deni Cho <denichodev@gmail.com>
2020-05-27 13:46:29 +0700

    Movements and collision

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,22 @@
 extends KinematicBody2D
 
+const ACCELERATION = 600
+const MAX_SPEED = 80
+const FRICTION = 680
+
 var velocity = Vector2.ZERO;
 
 func _physics_process(delta):
-	if (Input.is_action_pressed("ui_right")):
-		position.x += 10;
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	print (velocity);
+		
+	velocity = move_and_slide(velocity)
