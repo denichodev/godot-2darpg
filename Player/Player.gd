095b0ac421cf13c1481ee45fe259253164c64353
Deni Cho <denichodev@gmail.com>
2020-05-28 14:05:10 +0700

    Tilesets settings

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,17 +6,23 @@ const FRICTION = 680
 
 var velocity = Vector2.ZERO;
 
+onready var animationPlayer = $AnimationPlayer;
+onready var animationTree = $AnimationTree;
+onready var animationState = animationTree.get("parameters/playback");
+
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
+	var input_vector = Vector2.ZERO;
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+	input_vector = input_vector.normalized();
 
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	if (input_vector != Vector2.ZERO):
+		animationTree.set("parameters/Idle/blend_position", input_vector);
+		animationTree.set("parameters/Run/blend_position", input_vector);
+		animationState.travel("Run");
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
-	print (velocity);
+		animationState.travel("Idle");
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
 		
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity);
